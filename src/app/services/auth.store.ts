cd27408418f1caf5584bffc018233826f93ffc16
ofinochi <ofinochi@gmail.com>
2023-12-11 16:16:34 -0300

    Authentication support with Local Storage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,9 @@ import { map, shareReplay, tap } from 'rxjs/operators';
 import { User } from '../model/user';
 import { HttpClient } from '@angular/common/http';
 
+
+const AUTH_DATA = "auth_data";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,18 +22,27 @@ export class AuthStore {
 
     this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+    const authData = localStorage.getItem(AUTH_DATA);
+    if (authData) {
+      const user = JSON.parse(authData);
+      this.subject.next(user);
+    }
   }
 
   login(email: string, password: string): Observable<User> {
    return this.http.post<User>("/api/login", {email, password})
        .pipe(
-         tap(user => this.subject.next(user)),
+         tap(user => {
+           localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+           this.subject.next(user)}
+           ),
          shareReplay()
        );
   }
 
   logout() {
       this.subject.next(null);
+      localStorage.removeItem(AUTH_DATA);
   }
 
   
